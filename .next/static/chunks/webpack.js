c1e0e5681edcb959f1d49efd52a25556cb34477e
金晓翔 <>
2023-07-11 20:39:21 +0800

    delete redirect

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "23d51f0213db7b47"; }
+/******/ 		__webpack_require__.h = function() { return "020aeb087e8ba27f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
